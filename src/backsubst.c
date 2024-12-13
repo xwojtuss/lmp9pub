343d1d1a42ab0dc792f84e3c21142df546b0b87a
Wojciech Kornatowski <01197811@pw.edu.pl>
2024-12-13 11:33:15 +0100

    backsubst done

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,4 +1,6 @@
 #include "backsubst.h"
+
+int eliminate(Matrix *mat, Matrix *b);
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
@@ -8,21 +10,19 @@ int backsubst(Matrix *x, Matrix *mat, Matrix *b)
 {
     int n;
     
+	if (eliminate(mat,b))
+		return (1);
     n = mat->r;
     if (n != mat->c || n != b->r || n != x->r)
         return (2);
-
     for (int i = n - 1; i >= 0; i--)
     {
         if (mat->data[i][i] == 0)
             return (1);
         x->data[i][0] = b->data[i][0];
-
         for (int j = i + 1; j < n; j++)
             x->data[i][0] -= mat->data[i][j] * x->data[j][0];
-
         x->data[i][0] /= mat->data[i][i];
     }
-
     return (0);
 }
