87e512d97b9de20c6ba8e2ed1b02b99fdf20f08a
Wojciech Kornatowski <01197811@pw.edu.pl>
2024-12-13 11:28:48 +0100

    unfinished backsubstr

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,19 +4,25 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b)
+{
+    int n;
+    
+    n = mat->r;
+    if (n != mat->c || n != b->r || n != x->r)
+        return (2);
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (mat->data[i][i] == 0)
+            return (1);
+        x->data[i][0] = b->data[i][0];
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+        for (int j = i + 1; j < n; j++)
+            x->data[i][0] -= mat->data[i][j] * x->data[j][0];
 
-				return 0;
+        x->data[i][0] /= mat->data[i][i];
+    }
+
+    return (0);
 }
-
-
--- src/main.c
+++ src/main.c
@@ -15,12 +15,12 @@ int main(int argc, char ** argv) {
 	if (b == NULL) return -2;
 	printToScreen(A);
 	printToScreen(b);
-
+	printf("%f\n", A->data[1][0]);
 	res = eliminate(A,b);
 	x = createMatrix(b->r, 1);
 	if (x != NULL) {
 		res = backsubst(x,A,b);
-
+		printf("return value of res: %i\n", res);
 		printToScreen(x);
 	  freeMatrix(x);
 	} else {
